0a6ba21482cd54073d619d31c3ba470b647c66b9
SonderXiaoming <1316864423@qq.com>
2024-10-19 23:23:04 -0500

    Add files via upload

--- moyu.py
+++ moyu.py
@@ -1,29 +1,22 @@
-import io
-
-import base64
 import httpx
 
-from hoshino import Service, aiorequests
-from hoshino.typing import MessageSegment
+from hoshino import Service
+from nonebot import MessageSegment
 
-sv_help = '''
+sv_help = """
 每日摸鱼
 启用后会在每天早上发送一份摸鱼日历
 [@bot 摸鱼日历] （测试用）手动发送一份早报
-'''.strip()
+""".strip()
 
 sv = Service(
-    name='每日摸鱼',
-    enable_on_default=True,
-    visible=True,
-    bundle="娱乐",
-    help_=sv_help
+    name="每日摸鱼", enable_on_default=True, visible=True, bundle="娱乐", help_=sv_help
 )
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.6) ",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "zh-cn"
+    "Accept-Language": "zh-cn",
 }
 
 
@@ -31,21 +24,15 @@ async def get_calendar() -> str:
     async with httpx.AsyncClient(http2=True) as client:
         response = await client.get(
             url="https://api.j4u.ink/v1/store/other/proxy/remote/moyu.json",
-            headers=headers
+            headers=headers,
         )
     if response.is_error:
         raise ValueError(f"摸鱼日历获取失败，错误码：{response.status_code}")
     content = response.json()
-    image_url = content["data"]["moyu_url"]
-    try:
-        image_bytes = await (await aiorequests.get(image_url, headers=headers)).content
-        return f"base64://{base64.b64encode(io.BytesIO(image_bytes).getvalue()).decode()}"
-    except Exception as e:
-        sv.logger.critical(f"获取图片失败：{e}")
-        return ""
+    return content["data"]["img_url"]
 
 
-@sv.scheduled_job('cron', hour='11', jitter=50)
+@sv.scheduled_job("cron", hour="11", jitter=50)
 async def automoyu():
     moyu_img = await get_calendar()
     if not moyu_img:
@@ -55,7 +42,7 @@ async def automoyu():
     await sv.broadcast(message)
 
 
-@sv.on_fullmatch('摸鱼日历', only_to_me=True)
+@sv.on_fullmatch("摸鱼日历", only_to_me=True)
 async def handnews(bot, ev):
     moyu_img = await get_calendar()
     if not moyu_img:
